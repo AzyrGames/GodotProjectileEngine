7e97407d6e421bec0e60d9d746d0e826b3cb2d99
superhighlevel <superhighlive@gmail.com>
2025-05-30 15:09:18 +0700

    asdf

--- addons/GodotBulletHellEngine/Core/BulletTemplate/BulletTriggerCondition/bullet_trigger_condition.gd
+++ addons/GodotBulletHellEngine/Core/BulletTemplate/BulletTriggerCondition/bullet_trigger_condition.gd
@@ -5,6 +5,8 @@ class_name BulletTriggerCondition2D
 @export var is_active : bool = true
 @export var one_shot : bool = true
 
+@export var destroy_on_trigger : bool = true
+
 
 func check_trigger_condition(trigger_name: String, active_bullet_instances: Array[BulletInstance2D]) -> void:
 	pass
