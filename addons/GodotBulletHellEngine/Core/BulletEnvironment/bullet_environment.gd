c5d5a50cd34bd32a8de54cce8865dc47e251f86a
superhighlevel <superhighlive@gmail.com>
2025-06-08 08:40:43 +0700

    Move assign bullet environment to enter, exit tree function

--- addons/GodotBulletHellEngine/Core/BulletEnvironment/bullet_environment.gd
+++ addons/GodotBulletHellEngine/Core/BulletEnvironment/bullet_environment.gd
@@ -2,11 +2,16 @@ extends Node2D
 class_name BulletEnvironment2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _enter_tree() -> void:
+	if BulletHell.bullet_environment: return 
 	BulletHell.bullet_environment = self
-	pass # Replace with function body.
+	pass
+func _exit_tree() -> void:
+	BulletHell.bullet_environment = null
+	pass
 
+func _ready() -> void:
+	pass # Replace with function body.
 
 
 func _physics_process(delta: float) -> void:
@@ -25,6 +30,7 @@ func bullet_collided(bullet_area_rid: RID, shape_idx: int) -> void:
 		return
 
 	BulletHell.bullet_updater_2d_nodes[bullet_area_rid].process_bullet_collided(shape_idx)
+
 
 func get_bullet_damage(bullet_area_rid: RID) -> int:
 	if !BulletHell.bullet_updater_2d_nodes.has(bullet_area_rid): return 0
