66ee1567071cf7df3369c5b85bd638bd32f0fdb2
superhighlevel <superhighlive@gmail.com>
2025-06-11 18:08:50 +0700

    Refactor TSCCooldown

--- addons/godot_projectile_engine/core/timing_scheduler/timing_scheduler_component/tsc_cooldown.gd
+++ addons/godot_projectile_engine/core/timing_scheduler/timing_scheduler_component/tsc_cooldown.gd
@@ -1,24 +1,24 @@
 extends TimingSchedulerComponent
 class_name TSCCooldown
 
+## Cooldown timing scheduler component that waits for a fixed duration before completing
 
+## Duration of the cooldown in seconds
 @export var cooldown_duration : float = 1.0
 
-
+## Starts the cooldown timer with the next timing value
 func start_next_timing_value() -> void:
+	# Emit timed signal immediately since cooldown starts right away
 	tsc_timed.emit()
-	var _timing_value := get_next_timing_value()
-	if _timing_value <= 0.0:
+	
+	if cooldown_duration <= 0.0:
+		# If duration is 0 or negative, complete immediately
 		tsc_completed.emit()
 		return
-	timing_timer.start(_timing_value)
-	pass
+	
+	# Start the timer with the cooldown duration
+	timing_timer.start(cooldown_duration)
 
-
-func get_next_timing_value() -> float:
-	return cooldown_duration
-
-
+## Called when the cooldown timer completes
 func on_timing_timer_timeout() -> void:
 	tsc_completed.emit()
-	pass
