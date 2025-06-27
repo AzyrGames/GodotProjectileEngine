fb709c9dad99c829b6ba314d09a05cfaf08a112b
Lê Văn Hiếu <superhighlive@gmail.com>
2025-06-27 17:31:02 +0700

    fix double registering patterncomposer (#64)

--- addons/godot_projectile_engine/core/pattern_composer/pattern_composer.gd
+++ addons/godot_projectile_engine/core/pattern_composer/pattern_composer.gd
@@ -6,25 +6,22 @@ signal pattern_composed(_pattern_composer_pack: Dictionary)
 @export var composer_name : String = "":
 	set(value):
 		if value == composer_name: return
-		_reregister_pattern_composer(composer_name, value)
+		_register_pattern_composer(composer_name)
 		composer_name = value
 
 var pattern_composer_pack : Array[PatternComposerData]
-
-
 
 func _enter_tree() -> void:
 	_register_pattern_composer(composer_name)
 	pass
 
-
 func _exit_tree() -> void:
 	_deregister_pattern_composer(composer_name)
 	pass
 
-
 func _register_pattern_composer(_composer_name: String) -> void:
 	if _composer_name == "": return
+	_deregister_pattern_composer(_composer_name)
 	if ProjectileEngine.projectile_composer_nodes.has(_composer_name): 
 		print_debug("Composer name: ", _composer_name, " is existed: ", ProjectileEngine.projectile_composer_nodes.get(_composer_name))
 		return
@@ -42,14 +39,6 @@ func _deregister_pattern_composer(_composer_name: String) -> void:
 	pass
 
 
-func _reregister_pattern_composer(_old_composer_name: String, _new_composer_name: String) -> void:
-	_deregister_pattern_composer(_old_composer_name)
-	_register_pattern_composer(_new_composer_name)
-	
-	# print("re-registered: {0} -- {1}".format([_old_composer_name, _new_composer_name]))
-	pass
-
-
 func request_pattern(_pattern_composer_context : PatternComposerContext) -> Array:
 	pattern_composer_pack  = [PatternComposerData.new()]
 
@@ -59,5 +48,4 @@ func request_pattern(_pattern_composer_context : PatternComposerContext) -> Arra
 		pattern_composer_pack = pattern_component.process_pattern(pattern_composer_pack, _pattern_composer_context)
 	
 	pattern_composed.emit(pattern_composer_pack)
-
 	return pattern_composer_pack
