db84ed3f2eef30a7edec35fafa474e9f0e243d24
superhighlevel <superhighlive@gmail.com>
2025-07-16 11:18:21 +0700

    Add Pattern Composer Spread and Stack Unit Test

--- tests/pattern_composer/test_gdunit_pattern_composer.gd
+++ tests/pattern_composer/test_gdunit_pattern_composer.gd
@@ -3,7 +3,25 @@ extends GdUnitTestSuite
 
 func test_pattern_composer_simple() -> void:
 	var runner := scene_runner("uid://c100r43aw5b57")
-	await runner.simulate_frames(10)
+	await runner.simulate_frames(5)
+	assert_object(runner).is_not_null()
+	pass
+
+func test_pattern_composer_polygon() -> void:
+	var runner := scene_runner("uid://dlxrdpvh8mcpu")
+	await runner.simulate_frames(5)
+	assert_object(runner).is_not_null()
+	pass
+
+func test_pattern_composer_spread() -> void:
+	var runner := scene_runner("uid://dtmk56w332se2")
+	await runner.simulate_frames(5)
+	assert_object(runner).is_not_null()
+	pass
+	
+func test_pattern_composer_stack() -> void:
+	var runner := scene_runner("uid://yypoytt7an8h")
+	await runner.simulate_frames(5)
 	assert_object(runner).is_not_null()
 	pass
 
