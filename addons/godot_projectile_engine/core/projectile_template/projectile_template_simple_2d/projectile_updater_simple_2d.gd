887a04815e4816d7a4b7c269199af5dd25383712
Lê Văn Hiếu <superhighlive@gmail.com>
2025-07-17 16:04:17 +0700

    Set physics_engine to default and and collision_shape check (#138)

--- addons/godot_projectile_engine/core/projectile_template/projectile_template_simple_2d/projectile_updater_simple_2d.gd
+++ addons/godot_projectile_engine/core/projectile_template/projectile_template_simple_2d/projectile_updater_simple_2d.gd
@@ -39,10 +39,10 @@ func spawn_projectile_pattern(pattern_composer_pack: Array[PatternComposerData])
 			projectile_template_2d.skew,
 			_projectile_instance.global_position
 			)
-
-		PS.area_set_shape_transform(projectile_area_rid, projectile_pooling_index, _projectile_instance.transform)
-		PS.area_set_shape_disabled(projectile_area_rid, projectile_pooling_index, false)
-	
+		if projectile_template_2d.collision_shape:
+			PS.area_set_shape_transform(projectile_area_rid, projectile_pooling_index, _projectile_instance.transform)
+			PS.area_set_shape_disabled(projectile_area_rid, projectile_pooling_index, false)
+		
 		_projectile_instance.life_time_second = 0.0
 		_projectile_instance.life_distance = 0.0
 
@@ -100,7 +100,6 @@ func update_projectile_instances(delta: float) -> void:
 		_active_instances.append(projectile_instance_array[index])
 	if _active_instances.size() <= 0: return
 
-
 	# Update active projectile
 	for _active_instance : ProjectileInstanceSimple2D in _active_instances:
 
@@ -110,7 +109,12 @@ func update_projectile_instances(delta: float) -> void:
 			_active_instance.rotation, 
 			_active_instance.global_position
 			)
-
-		PS.area_set_shape_transform(projectile_area_rid, _active_instance.area_index, _active_instance.transform)
+		# if _active_instance.area_rid:
+		if projectile_template_2d.collision_shape:
+			PS.area_set_shape_transform(
+				projectile_area_rid, 
+				_active_instance.area_index, 
+				_active_instance.transform
+				)
 
 #endregion
