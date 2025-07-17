b62949bfc82aa3948e72cb199b048128d038b8ed
Lê Văn Hiếu <superhighlive@gmail.com>
2025-07-17 16:25:32 +0700

    Add ProjectileTemplateNode2D file path check (#139)

--- addons/godot_projectile_engine/core/projectile_template/projectile_template_node_2d/entities/projectile_node_manager.gd
+++ addons/godot_projectile_engine/core/projectile_template/projectile_template_node_2d/entities/projectile_node_manager.gd
@@ -13,13 +13,24 @@ var projectile_node_2d_packedscene : PackedScene
 
 var _projectile_node_2d : Projectile2D
 
+var _is_valid_projectile_node_2d : bool = false
 
 
 func setup_projectile_manager() -> void:
 	if !is_instance_valid(projectile_template_2d):
 		return
 	projectile_template_2d = projectile_template_2d as ProjectileTemplateNode2D
-	projectile_node_2d_packedscene = load(projectile_template_2d.projectile_2d_path)
+	projectile_node_2d_packedscene = _load_projectile_node(projectile_template_2d.projectile_2d_path)
+	if !is_instance_valid(projectile_node_2d_packedscene):
+		print_debug(projectile_node_2d_packedscene, " is not valid!")
+		_is_valid_projectile_node_2d = false
+		return
+	var _instantiate_node = projectile_node_2d_packedscene.instantiate() 
+	if !_instantiate_node is Projectile2D:
+		print_debug(_instantiate_node, " is not Projectile2D")
+		_is_valid_projectile_node_2d = false
+		return
+	_is_valid_projectile_node_2d = true
 	create_projectile_pool()
 	pass
 
@@ -37,10 +48,14 @@ func create_projectile_pool() -> void:
 		add_child(_projectile_node_2d, true)
 		_projectile_node_2d.set_owner(self)
 		projectile_node_array.append(_projectile_node_2d)
+	
 	pass
 
 
 func spawn_projectile_pattern(pattern_composer_pack: Array[PatternComposerData]) -> void:
+	if !_is_valid_projectile_node_2d:
+		push_warning("ProjectileNode2D is not vailid")
+		return
 	if projectile_max_pooling > 0:
 		for _pattern_composer_data : PatternComposerData in pattern_composer_pack:
 			_projectile_node_2d = projectile_node_array[projectile_pooling_index]
@@ -60,3 +75,14 @@ func spawn_projectile_pattern(pattern_composer_pack: Array[PatternComposerData])
 			_projectile_node_2d.apply_pattern_composer_data(_pattern_composer_data)
 			add_child(_projectile_node_2d, true)
 			_projectile_node_2d.owner = self
+
+func _load_projectile_node(_file_path: String) -> PackedScene:
+	if _file_path == "":
+		print_debug("ProjectileTemplateNode file path is null")
+		return null
+	var _packed_projectile_node : PackedScene = load(_file_path)
+	if !_packed_projectile_node:
+		print_debug(_file_path ," is not a valid file path: ")
+		return null
+	return _packed_projectile_node
+	pass
