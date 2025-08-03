3255e886ef8e9581171de572a16da59c6003f16c
superhighlevel <superhighlive@gmail.com>
2025-08-03 22:38:11 +0700

    small update

--- addons/godot_projectile_engine/core/pattern_composer/component/PCCSingle2D.gd
+++ addons/godot_projectile_engine/core/pattern_composer/component/PCCSingle2D.gd
@@ -1,12 +1,16 @@
-## Projectile Pattern Component Base
 @icon("uid://cd2yhrjd4vb5f")
 extends PatternComposerComponent
 class_name PCCSingle2D
 
+
+## Pattern Composer Component Single 2D.[br]
+## Set [param direction] and [param rotation] for each incoming [PatternComposerData]
+
+
 enum DirectionType {
 	INHERIT, ## Use the current ProjectileSpawner direction
-	FIXED, ## Overwrite the direction with `fixed_direction`
-	TARGET_GROUP, ## Direction from ProjectileSpawner to the target Position
+	FIXED, ## Overwrite the direction with [param fixed_direction]
+	TARGET_GROUP, ## 
 	MOUSE, ## Direction from ProjectileSpawner to the mouse position
 }
 
@@ -15,41 +19,42 @@ enum RotationProcessMode {
 	TICKS ## Apply rotation speed each time pattern was processed
 }
 
-## Type of direction
-@export var direction_type : DirectionType
+var _request_tick: bool = false
 
-## Normalized fixed direction for Fixed DirectionType
-@export var fixed_direction : Vector2 = Vector2.RIGHT
-## Normalized target group name for TARGET DirectionType
-@export var target_group : String
-@export var group_selection : ProjectileEngine.TargetGroupSelection
+## Type of direction will be processed
+@export var direction_type: DirectionType
+## [enum DirectionType][code].FIXED[/code][br]
+## Normalized fixed direction
+@export var fixed_direction: Vector2 = Vector2.RIGHT
+## Target group name
+@export var target_group: String
+## Group node selection method 
+@export var group_selection: ProjectileEngine.TargetGroupSelection
 ## Direction Rotation as degrees
-@export_range(-360, 360, 0.1, "radians_as_degrees") var rotation : float = 0
+@export_range(-360, 360, 0.1, "radians_as_degrees") var rotation: float = 0
 
 ## Direction Rotation Speed as degrees
-@export var rotation_speed : float = 0
+@export var rotation_speed: float = 0
 
 # @export var rotation_speed_curve : CurveTexture
-@export var rotation_process_mode : RotationProcessMode
+## rotation process update mode
+@export var rotation_process_mode: RotationProcessMode
 
 @export_group("Random")
-@export var target_group_choice : PackedStringArray
+## Array of target group names
+@export var target_groups: PackedStringArray
 
 ## Random rotation value
-@export var rotation_random : Vector3
+@export var rotation_random: Vector3
 ## Random rotation speed value
-@export var rotation_speed_random : Vector3
-var angle_random : float
+@export var rotation_speed_random: Vector3
 
-var _request_tick : bool = false
-var _rng : RandomNumberGenerator
-var _target_nodes : Array[Node2D]
-# var _target_node : Node2D
-var _final_rotation : float
+var _target_nodes: Array[Node2D]
+var _final_rotation: float
 
 var _nearest_target: Node2D
 var _nearest_distance: float
-var _distance : float
+var _distance: float
 
 
 func _ready() -> void:
@@ -59,9 +64,9 @@ func _ready() -> void:
 
 func process_pattern(
 	_pattern_composer_pack: Array[PatternComposerData],
-	_pattern_composer_context : PatternComposerContext
+	_pattern_composer_context: PatternComposerContext
 	) -> Array:
-	for _pattern_composer_data : PatternComposerData in _pattern_composer_pack:
+	for _pattern_composer_data: PatternComposerData in _pattern_composer_pack:
 		_final_rotation = _pattern_composer_data.rotation
 		if rotation != 0.0:
 			_final_rotation += rotation
@@ -82,7 +87,7 @@ func process_pattern(
 					base_direction.rotated(_final_rotation).normalized()
 					continue
 		
-				if target_group_choice.is_empty():
+				if target_groups.is_empty():
 					_target_nodes = ProjectileEngine.get_valid_target_group_nodes(target_group)
 				else:
 					_update_target_nodes_group_choice()
@@ -104,7 +109,7 @@ func process_pattern(
 	return _pattern_composer_pack
 
 
-func update(_pattern_composer_pack : Array[PatternComposerData]) -> void:
+func update(_pattern_composer_pack: Array[PatternComposerData]) -> void:
 	for _pattern_composer_data in _pattern_composer_pack:
 		if !_pattern_composer_data: return
 		match rotation_process_mode:
@@ -116,8 +121,9 @@ func update(_pattern_composer_pack : Array[PatternComposerData]) -> void:
 				_pattern_composer_data.rotation += deg_to_rad(rotation_speed) * get_physics_process_delta_time()
 	_request_tick = false
 
+
 # Search and return the first Node2D in a Group, return null if not founded
-func get_first_node2d_group(_group_name : String) -> Node2D:
+func get_first_node2d_group(_group_name: String) -> Node2D:
 	if _group_name == null:
 		return
 	return
@@ -134,14 +140,14 @@ func get_mouse_position() -> Vector2:
 func _update_target_nodes_group_choice() -> void:
 	# Todo: Cache Group choice to update when changed
 	_target_nodes.clear()
-	for _target_group in target_group_choice:
+	for _target_group in target_groups:
 		for _node: Node in get_tree().get_nodes_in_group(_target_group):
 			if !_node is Node2D or !is_instance_valid(_node): continue
 			if _target_nodes.has(_node): continue
 			_target_nodes.append(_node)
 
 
-func get_target_position(_pattern_composer_data : PatternComposerData) -> Vector2:
+func get_target_position(_pattern_composer_data: PatternComposerData) -> Vector2:
 	match group_selection:
 		ProjectileEngine.TargetGroupSelection.FIRST:
 			return _target_nodes[0].global_position
