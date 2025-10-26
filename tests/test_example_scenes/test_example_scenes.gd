82884bd5c32ed61a734ed9108206a60febbc1416
superhighlevel <superhighlive@gmail.com>
2025-10-26 14:33:23 +0700

    Update tests

--- tests/test_example_scenes/test_example_scenes.gd
+++ tests/test_example_scenes/test_example_scenes.gd
@@ -4,16 +4,22 @@ var runner: GdUnitSceneRunner
 
 func test_example_scene_1() -> void:
 	runner = null
-	runner = scene_runner("uid://cwlrovd240rt2")
-	# runner._scene_auto_free = true
+	var _scene : Node2D = load("uid://cwlrovd240rt2").instantiate()
+	## Workaround for A problem
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
+	runner = scene_runner(_scene)
+	runner._scene_auto_free = true
 
 	await runner.simulate_frames(10)
 	assert_object(runner).is_not_null()
 	pass
 
 func test_example_scene_2() -> void:
+	var _scene : Node2D = load("uid://b34nudu7t8gwd").instantiate()
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
+
 	runner = null
-	runner = scene_runner("uid://b34nudu7t8gwd")
+	runner = scene_runner(_scene)
 	runner._scene_auto_free = true
 
 	await runner.simulate_frames(10)
@@ -23,33 +29,43 @@ func test_example_scene_2() -> void:
 
 func test_example_scene_3() -> void:
 	runner = null
-	runner = scene_runner("uid://rxicnemi55bq")
-	runner._scene_auto_free = true
+	var _scene : Node2D = load("uid://rxicnemi55bq").instantiate()
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
 
+	runner = scene_runner(_scene)
+	runner._scene_auto_free = true
 	await runner.simulate_frames(10)
 	assert_object(runner).is_not_null()
 	pass
 	
 func test_example_scene_4() -> void:
+	var _scene : Node2D = load("uid://4jgbpsiaa8uu").instantiate()
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
+
 	runner = null
-	runner = scene_runner("uid://4jgbpsiaa8uu")
+	runner = scene_runner(_scene)
 	runner._scene_auto_free = true
 	await runner.simulate_frames(10)
 	assert_object(runner).is_not_null()
 	pass
 	
 func test_example_scene_5() -> void:
-	runner = null
-	runner = scene_runner("uid://bccgqsjl1b74f")
-	runner._scene_auto_free = true
+	var _scene : Node2D = load("uid://bccgqsjl1b74f").instantiate()
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
 
+	runner = null
+	runner = scene_runner(_scene)
+	runner._scene_auto_free = true
 	await runner.simulate_frames(10)
 	assert_object(runner).is_not_null()
 	pass
 	
 func test_example_scene_6() -> void:
+	var _scene : Node2D = load("uid://dactafr7x6iw3").instantiate()
+	_scene.process_mode = Node.PROCESS_MODE_DISABLED
+
 	runner = null
-	runner = scene_runner("uid://dactafr7x6iw3")
+	runner = scene_runner(_scene)
 	runner._scene_auto_free = true
 
 	await runner.simulate_frames(10)
