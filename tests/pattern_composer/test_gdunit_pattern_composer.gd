3de9f0d428a8e9c4760f3c9146ac97013155ccf0
superhighlevel <superhighlive@gmail.com>
2025-10-26 12:54:23 +0700

    Update Tests

--- tests/pattern_composer/test_gdunit_pattern_composer.gd
+++ tests/pattern_composer/test_gdunit_pattern_composer.gd
@@ -1,57 +1,73 @@
 extends GdUnitTestSuite
 
-
+var runner : GdUnitSceneRunner
 func test_pattern_composer_single() -> void:
-	var runner := scene_runner("uid://c100r43aw5b57")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://c100r43aw5b57")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_polygon() -> void:
-	var runner := scene_runner("uid://dlxrdpvh8mcpu")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://dlxrdpvh8mcpu")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_spread() -> void:
-	var runner := scene_runner("uid://dtmk56w332se2")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://dtmk56w332se2")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_stack() -> void:
-	var runner := scene_runner("uid://yypoytt7an8h")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://yypoytt7an8h")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_shape_2d() -> void:
-	var runner := scene_runner("uid://vnh02vjautdp")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://vnh02vjautdp")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_custom_shape_2d() -> void:
-	var runner := scene_runner("uid://s3visbj8dq43")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://s3visbj8dq43")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_loop() -> void:
-	var runner := scene_runner("uid://vuaj15gpwguh")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://vuaj15gpwguh")
 	runner._scene_auto_free = false
 	runner.simulate_frames(5)
 	assert_object(runner).is_not_null()
 	pass
 
 func test_pattern_composer_group() -> void:
-	var runner := scene_runner("uid://crdq3kd2tjy82")
+	runner = null
+	ProjectileEngine.clear_all_projectiles()
+	runner = scene_runner("uid://crdq3kd2tjy82")
 	runner.simulate_frames(5)
 	runner._scene_auto_free = false
 	assert_object(runner).is_not_null()
