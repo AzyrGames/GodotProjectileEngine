1428c6d43979165e8d24370318a579af6a1ba957
superhighlevel <superhighlive@gmail.com>
2025-12-08 20:02:13 +0700

    remove GUT addon and test files

--- tests/test_projectile_wrapper/test_projectile_wrapper_scene.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	ProjectileEngine.activate_all_projectile_wrappers("")
-	ProjectileEngine.activate_all_projectile_wrappers("wrong_wrapper")
-	ProjectileEngine.activate_all_projectile_wrappers("test_wrapper_node_1")
-	ProjectileEngine.deactivate_all_projectile_wrappers("test_wrapper_node_1")
